cbbd5a1cc310c3a4fd8890518f10be700065df08
BicicletariaMayBike <douglasomendes2510@gmail.com>
2026-06-08 21:16:01 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'mypdv-saas-v1-0-4-cache';
+const APP_SHELL = [
+  './', './index.html', './style.css?v=104', './app.js?v=104', './pwa.js?v=104', './manifest.json?v=104',
+  './icons/icon-192.png','./icons/icon-512.png','./icons/maybike-logo.png','./icons/maybike-logo-horizontal.png','./icons/maybike-logo-print.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => caches.open(CACHE_NAME)).then(cache => cache.addAll(APP_SHELL)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.pathname.endsWith('/app.js') || url.pathname.endsWith('/index.html') || url.pathname.endsWith('/service-worker.js')) {
+    event.respondWith(fetch(event.request, {cache:'no-store'}).catch(() => caches.match(event.request)));
+    return;
+  }
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+    return response;
+  }).catch(() => caches.match(event.request).then(resp => resp || caches.match('./index.html'))));
+});
